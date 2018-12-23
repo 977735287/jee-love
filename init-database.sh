0d2f8ccb6ab4afd1d0faf81bc2a44cc135c7bd1d
黄砷才 <shencai.huang@hand-china.com>
2018-12-23 20:38:56 +0800

    数据库脚本初始化 20181216

---
+++ init-database.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p bin
+if [ ! -f bin/choerodon-tool-liquibase.jar ]
+then
+    curl https://oss.sonatype.org/content/groups/public/io/choerodon/choerodon-tool-liquibase/0.5.2.RELEASE/choerodon-tool-liquibase-0.5.2.RELEASE.jar -o ./bin/choerodon-tool-liquibase.jar
+fi
+java -Dspring.datasource.url="jdbc:mysql://127.0.0.1:3306/jee_love?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
+ -Dspring.datasource.username=root \
+ -Dspring.datasource.password=root \
+ -Ddata.drop=false -Ddata.init=true \
+ -Ddata.dir=./src/main/resources \
+ -jar ./bin/choerodon-tool-liquibase.jar
